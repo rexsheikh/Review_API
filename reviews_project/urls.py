be20dbfa3d4acfc271e2926a26a8c5944fd136bd
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-03-16 11:42:21 -0400

    renamed project. created database and reconfigured necessary fiels.

--- reviews_project/urls.py
+++ reviews_project/urls.py
@@ -1,4 +1,4 @@
-"""products_project URL Configuration
+"""reviews_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
